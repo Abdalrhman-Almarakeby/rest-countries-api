ee3f71b95b3b4dc19863e769fbd226bd2570bb54
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2023-08-07 19:39:59 +0300

    custmize the container to be centered

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
-        Blue: "hsl(209, 23%, 22%)",
-        DarkBlue: "hsl(207, 26%, 17%)",
+        blue: "hsl(209, 23%, 22%)",
+        darkBlue: "hsl(207, 26%, 17%)",
         veryDarkBlue: "hsl(200, 15%, 8%)",
         darkGray: "hsl(0, 0%, 52%)",
         lightGray: "hsl(0, 0%, 98%)",
