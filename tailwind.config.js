4aace5ff7e2ef45a943074cf39a3e6b72f1613ed
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2023-08-07 15:20:20 +0300

    Add the custom colors on the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Blue: "hsl(209, 23%, 22%)",
+        DarkBlue: "hsl(207, 26%, 17%)",
+        veryDarkBlue: "hsl(200, 15%, 8%)",
+        darkGray: "hsl(0, 0%, 52%)",
+        lightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
