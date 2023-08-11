1285c503de86286fef9d3cc43a603cf79254f354
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2023-08-11 14:47:40 +0300

    Handel the search input

--- src/JS/main.js
+++ src/JS/main.js
@@ -11,7 +11,6 @@ function toggleDarkMode() {
   changeModeText();
 }
 
-
 function changeModeText() {
   const modeText = darkModeBtn.querySelector("span");
   const isDarkMode = page.classList.contains("dark");
@@ -39,11 +38,11 @@ const cardsContainer = document.getElementById("cards-container")
 const countryCardTemplate = document.querySelector("[data-country-template]")
 
 function createCountryCard(countryObject) {
-  const { name, flags, population, region, capital } = countryObject
+  const { name, flags, population, region, capitals } = countryObject
 
-  //!!!!!!!!!!!!!!!!!!!!//
-  if (name.common === "Israel") { return null }
-  //!!!!!!!!!!!!!!!!!!!!//
+  // //!!!!!!!!!!!!!!!!!!!!//
+  // if (name.common === "Israel") { return null }
+  // //!!!!!!!!!!!!!!!!!!!!//
 
   const card = countryCardTemplate.content.cloneNode(true).children[0]
   card.title = name.common;
@@ -60,11 +59,11 @@ function createCountryCard(countryObject) {
 
   card.querySelector("[data-population] span").textContent = population.toLocaleString();
   card.querySelector("[data-region] span").textContent = region;
-  card.querySelector("[data-capital] span").textContent = name.common === "Palestine" ? "Jerusalem" : capital;
+  card.querySelector("[data-capital] span").textContent = name.common === "Palestine" ? "Jerusalem" : capitals;
 
   cardsContainer.append(card)
 
-  return { name: name.common, region: region, element: card }
+  return { name: name.common, region: region, capitals: capitals, element: card }
 }
 
 let countries = []
@@ -72,7 +71,9 @@ let countries = []
 fetch("https://restcountries.com/v3.1/all")
   .then(response => response.json())
   .then(data => {
-    data.forEach((countryObject) => countries.push(createCountryCard(countryObject)))
+    data.forEach((countryObject) => {
+      countries.push(createCountryCard(countryObject));
+    })
   })
 ///////////////////////////////////////////
 // handle filter by region
@@ -86,6 +87,22 @@ regionFilter.addEventListener("input", (e) => {
       return
     }
     const isVisible = country.region === value
+    if (!isVisible) {
+      country.element.style.display = "none"
+    } else {
+      country.element.style.display = "block"
+    }
+  })
+})
+
+// Handel search input 
+const searchInput = document.getElementById("search-input")
+
+searchInput.addEventListener("input", (e) => {
+  countries.forEach((country) => {
+    const value = e.target.value.trim()
+
+    const isVisible = country.name.toLowerCase().includes(value.toLowerCase()) //|| country.capitals.forEach((capital) => { capital.includes(value) })
     if (!isVisible) {
       country.element.style.display = "none"
     } else {
